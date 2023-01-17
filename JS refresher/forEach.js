2314927401b777571397e267b2cb2e9ad31155f2
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-01-17 18:49:39 +1300

    add an example of uing forEach in array

---
+++ JS refresher/forEach.js
@@ -0,0 +1,4 @@
+const ninjaTurtles = ["Donatello", "Michaelangelo", "Leonardo", "Raphael"]
+
+ninjaTurtles.forEach(t => console.log(t))
+ninjaTurtles.forEach(t => console.log(`Index ${ninjaTurtles.indexOf(t)} is ${t}.`))
