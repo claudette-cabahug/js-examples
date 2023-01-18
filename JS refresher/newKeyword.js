3eb4976558c8e493c056d003e337196b839f3cbf
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-01-19 12:34:01 +1300

    access property using the new instance of the object

--- JS refresher/newKeyword.js
+++ JS refresher/newKeyword.js
@@ -11,6 +11,11 @@ var car1 = new Car("BMW", 539, "Jeck-jeck")
 var car2 = new Car("Mercedes-Benz", 503, "Claudette")
 var car3 = new Car("Fiat", 166, "Rodney")
 
-console.log(car1);
-console.log(car2);
-console.log(car3);
+// console.log(car1);
+// console.log(car2);
+// console.log(car3);
+
+car1.getDetails();
+console.log(car1.owner);
+car2.getDetails();
+car3.getDetails();
