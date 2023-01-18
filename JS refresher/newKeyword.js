b90ccd08e1b6d77ce08b278c6a22befdec4b8a2e
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-01-19 11:41:02 +1300

    create an object using 'this' and 'new' keywords and reuse it

---
+++ JS refresher/newKeyword.js
@@ -0,0 +1,16 @@
+var Car = function (make, model, owner) {
+  this.make = make,
+  this.model = model,
+  this.owner = owner,
+  this.getDetails = function () {
+    console.log(`The owner of the ${this.make} ${this.model} is ${this.owner}.`)
+  }
+}
+
+var car1 = new Car("BMW", 539, "Jeck-jeck")
+var car2 = new Car("Mercedes-Benz", 503, "Claudette")
+var car3 = new Car("Fiat", 166, "Rodney")
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
