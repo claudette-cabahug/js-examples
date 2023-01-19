aaa2b516022808f7bdd38d8e6aa8499b493b2245
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-01-20 12:19:04 +1300

    destructure an object

---
+++ JS refresher/destructure.js
@@ -0,0 +1,13 @@
+const student = {
+  firstName: "Claudette",
+  age: 40,
+  city: "Wellington",
+  gender: "female"
+}
+
+var {firstName, age, city, gender} = student
+
+console.log(firstName);
+console.log(age);
+console.log(city);
+console.log(gender);
