3b6f6d87f8b7cc25ec77bc136e96c041cae67128
Claudette Cabahug <claudette.cabahug@outlook.com>
2023-01-20 12:24:20 +1300

    destructure an array

--- JS refresher/destructure.js
+++ JS refresher/destructure.js
@@ -1,3 +1,4 @@
+//object
 const student = {
   firstName: "Claudette",
   age: 40,
@@ -7,7 +8,16 @@ const student = {
 
 var {firstName, age, city, gender} = student
 
-console.log(firstName);
-console.log(age);
-console.log(city);
-console.log(gender);
+// console.log(firstName);
+// console.log(age);
+// console.log(city);
+// console.log(gender);
+
+//array
+const country = ["Philippines", "Filipino", "Asia"]
+
+var [countryName, nationals, continent] = country
+
+console.log(countryName);
+console.log(nationals);
+console.log(continent);
